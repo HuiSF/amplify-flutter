95b0727416454fbca9af778d39a6030ddfef4d2e
Hui Zhao <zhohz@amazon.com>
2022-03-14 16:58:02 -0700

    Optimize shell scripts driving integration tests
    - Make device id and cloud sync configurable with melos run
    - Fail if one of the tests failed

--- build-support/integ_test_android.sh
+++ build-support/integ_test_android.sh
@@ -5,24 +5,93 @@ if [ ! -d android ]; then
     exit
 fi
 
+DEFAULT_DEVICE_ID="sdk"
+DEFAULT_ENABLE_CLOUD_SYNC="true"
+
+while [ $# -gt 0 ]; do
+    case "$1" in
+        -d|--device-id)
+            deviceId="$2"
+            ;;
+        -ec|--enable-cloud-sync)
+            case "$2" in
+                true|false)
+                    enableCloudSync="$2"
+                ;;
+                *)
+                    echo "Invalid value for $1"
+                    exit 1
+            esac
+            ;;
+        *)
+            echo "Invalid arguments"
+            exit 1
+    esac
+    shift
+    shift
+done
+
+deviceId=${deviceId:-$DEFAULT_DEVICE_ID}
+enableCloudSync=${enableCloudSync:-$DEFAULT_ENABLE_CLOUD_SYNC}
+
+declare -a testsList
+declare -a resultsList
+
 TARGET=integration_test/main_test.dart
 if [ ! -e $TARGET ]; then
     echo "$TARGET file not found" >&2
     exit
 fi
 
-flutter test \
+testsList+=("$TARGET")
+if flutter test \
     --no-pub \
-    -d sdk \
-    $TARGET
+    -d $deviceId \
+    $TARGET; then
+    resultsList+=(0)
+else
+    resultsList+=(1)
+fi
+
+
 
 TEST_ENTRIES=`ls integration_test/separate_integration_tests/*.dart`
 for ENTRY in $TEST_ENTRIES
 do
-    echo "Run $ENTRY WITH API Sync"
-    flutter test \
+    testsList+=("$ENTRY")
+    if [ $enableCloudSync == "true" ]
+    then
+        echo "Run $ENTRY WITH API Sync"
+    else
+        echo "Run $ENTRY WITHOUT API Sync"
+    fi
+
+    if flutter test \
         --no-pub \
-        --dart-define ENABLE_CLOUD_SYNC=true \
-        -d sdk \
-        $ENTRY
+        --dart-define ENABLE_CLOUD_SYNC=$enableCloudSync \
+        -d $deviceId \
+        $ENTRY; then
+        resultsList+=(0)
+    else
+        resultsList+=(1)
+    fi
 done
+
+testFailure=0
+for i in "${!testsList[@]}"; do
+    resultPrint=""
+    if [ "${resultsList[i]}" == 0 ]
+    then
+        echo "✅ ${testsList[i]}"
+    else
+        testFailure=1
+        echo "❌ ${testsList[i]}"
+    fi
+done
+
+if [ $testFailure -eq 1 ]
+then
+  exit 1
+else
+  exit 0
+fi
